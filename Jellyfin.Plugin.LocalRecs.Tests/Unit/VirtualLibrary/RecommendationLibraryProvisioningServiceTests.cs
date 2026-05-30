4fb05107d846a7d83be9889bf1cf0e5b1391c041
Christian Trab <christianmtrab@gmail.com>
2026-05-30 15:36:22 +0200

    Disable download artwork, disable tvmd at metadata provider, disable chapter image extraction, set AutoCreateRecommendationLibraries & AutoManageLibraryPermissions  to false as defalt

--- Jellyfin.Plugin.LocalRecs.Tests/Unit/VirtualLibrary/RecommendationLibraryProvisioningServiceTests.cs
+++ Jellyfin.Plugin.LocalRecs.Tests/Unit/VirtualLibrary/RecommendationLibraryProvisioningServiceTests.cs
@@ -31,38 +31,38 @@ namespace Jellyfin.Plugin.LocalRecs.Tests.Unit.VirtualLibrary
         {
             using (UsePluginConfiguration(config => config.AutoCreateRecommendationLibraries = true))
             {
-            var basePath = CreateBasePath();
-            var userId = Guid.NewGuid();
-            Directory.CreateDirectory(Path.Combine(basePath, userId.ToString(), "movies"));
-            Directory.CreateDirectory(Path.Combine(basePath, userId.ToString(), "tv"));
+                var basePath = CreateBasePath();
+                var userId = Guid.NewGuid();
+                Directory.CreateDirectory(Path.Combine(basePath, userId.ToString(), "movies"));
+                Directory.CreateDirectory(Path.Combine(basePath, userId.ToString(), "tv"));
 
-            var mockLibraryManager = new Mock<ILibraryManager>();
-            mockLibraryManager.Setup(m => m.GetVirtualFolders()).Returns(new List<VirtualFolderInfo>());
+                var mockLibraryManager = new Mock<ILibraryManager>();
+                mockLibraryManager.Setup(m => m.GetVirtualFolders()).Returns(new List<VirtualFolderInfo>());
 
-            var service = CreateService(basePath, mockLibraryManager.Object, Mock.Of<IUserManager>());
-            var user = new User("Alice", "Default", "Default") { Id = userId };
+                var service = CreateService(basePath, mockLibraryManager.Object, Mock.Of<IUserManager>());
+                var user = new User("Alice", "Default", "Default") { Id = userId };
 
-            await service.EnsureLibrariesForUserAsync(user);
+                await service.EnsureLibrariesForUserAsync(user);
 
-            mockLibraryManager.Verify(
-                m => m.AddVirtualFolder(
-                    "Alice's Recommended Movies",
-                    CollectionTypeOptions.movies,
-                    It.Is<LibraryOptions>(options =>
-                        options.PathInfos != null &&
-                        options.PathInfos.Any(path => path.Path.EndsWith("movies", StringComparison.OrdinalIgnoreCase))),
-                    false),
-                Times.Once);
+                mockLibraryManager.Verify(
+                    m => m.AddVirtualFolder(
+                        "Alice's Recommended Movies",
+                        CollectionTypeOptions.movies,
+                        It.Is<LibraryOptions>(options =>
+                            options.PathInfos != null &&
+                            options.PathInfos.Any(path => path.Path.EndsWith("movies", StringComparison.OrdinalIgnoreCase))),
+                        false),
+                    Times.Once);
 
-            mockLibraryManager.Verify(
-                m => m.AddVirtualFolder(
-                    "Alice's Recommended TV",
-                    CollectionTypeOptions.tvshows,
-                    It.Is<LibraryOptions>(options =>
-                        options.PathInfos != null &&
-                        options.PathInfos.Any(path => path.Path.EndsWith("tv", StringComparison.OrdinalIgnoreCase))),
-                    false),
-                Times.Once);
+                mockLibraryManager.Verify(
+                    m => m.AddVirtualFolder(
+                        "Alice's Recommended TV",
+                        CollectionTypeOptions.tvshows,
+                        It.Is<LibraryOptions>(options =>
+                            options.PathInfos != null &&
+                            options.PathInfos.Any(path => path.Path.EndsWith("tv", StringComparison.OrdinalIgnoreCase))),
+                        false),
+                    Times.Once);
             }
         }
 
@@ -71,14 +71,14 @@ namespace Jellyfin.Plugin.LocalRecs.Tests.Unit.VirtualLibrary
         {
             using (UsePluginConfiguration(config => config.AutoCreateRecommendationLibraries = true))
             {
-            var basePath = CreateBasePath();
-            var userId = Guid.NewGuid();
-            var moviePath = Path.Combine(basePath, userId.ToString(), "movies");
-            var tvPath = Path.Combine(basePath, userId.ToString(), "tv");
-            Directory.CreateDirectory(moviePath);
-            Directory.CreateDirectory(tvPath);
+                var basePath = CreateBasePath();
+                var userId = Guid.NewGuid();
+                var moviePath = Path.Combine(basePath, userId.ToString(), "movies");
+                var tvPath = Path.Combine(basePath, userId.ToString(), "tv");
+                Directory.CreateDirectory(moviePath);
+                Directory.CreateDirectory(tvPath);
 
-            var existingFolders = new List<VirtualFolderInfo>
+                var existingFolders = new List<VirtualFolderInfo>
             {
                 new VirtualFolderInfo
                 {
@@ -94,21 +94,21 @@ namespace Jellyfin.Plugin.LocalRecs.Tests.Unit.VirtualLibrary
                 }
             };
 
-            var mockLibraryManager = new Mock<ILibraryManager>();
-            mockLibraryManager.Setup(m => m.GetVirtualFolders()).Returns(existingFolders);
+                var mockLibraryManager = new Mock<ILibraryManager>();
+                mockLibraryManager.Setup(m => m.GetVirtualFolders()).Returns(existingFolders);
 
-            var service = CreateService(basePath, mockLibraryManager.Object, Mock.Of<IUserManager>());
-            var user = new User("Alice", "Default", "Default") { Id = userId };
+                var service = CreateService(basePath, mockLibraryManager.Object, Mock.Of<IUserManager>());
+                var user = new User("Alice", "Default", "Default") { Id = userId };
 
-            await service.EnsureLibrariesForUserAsync(user);
+                await service.EnsureLibrariesForUserAsync(user);
 
-            mockLibraryManager.Verify(
-                m => m.AddVirtualFolder(
-                    It.IsAny<string>(),
-                    It.IsAny<CollectionTypeOptions>(),
-                    It.IsAny<LibraryOptions>(),
-                    It.IsAny<bool>()),
-                Times.Never);
+                mockLibraryManager.Verify(
+                    m => m.AddVirtualFolder(
+                        It.IsAny<string>(),
+                        It.IsAny<CollectionTypeOptions>(),
+                        It.IsAny<LibraryOptions>(),
+                        It.IsAny<bool>()),
+                    Times.Never);
             }
         }
 
@@ -117,16 +117,16 @@ namespace Jellyfin.Plugin.LocalRecs.Tests.Unit.VirtualLibrary
         {
             using (UsePluginConfiguration(config => config.AutoManageLibraryPermissions = true))
             {
-            var basePath = CreateBasePath();
-            var user1Id = Guid.NewGuid();
-            var user2Id = Guid.NewGuid();
-            var user1MovieFolderId = Guid.NewGuid().ToString();
-            var user2MovieFolderId = Guid.NewGuid().ToString();
+                var basePath = CreateBasePath();
+                var user1Id = Guid.NewGuid();
+                var user2Id = Guid.NewGuid();
+                var user1MovieFolderId = Guid.NewGuid().ToString();
+                var user2MovieFolderId = Guid.NewGuid().ToString();
 
-            var user1MoviePath = Path.Combine(basePath, user1Id.ToString(), "movies");
-            var user2MoviePath = Path.Combine(basePath, user2Id.ToString(), "movies");
+                var user1MoviePath = Path.Combine(basePath, user1Id.ToString(), "movies");
+                var user2MoviePath = Path.Combine(basePath, user2Id.ToString(), "movies");
 
-            var existingFolders = new List<VirtualFolderInfo>
+                var existingFolders = new List<VirtualFolderInfo>
             {
                 new VirtualFolderInfo
                 {
@@ -142,30 +142,30 @@ namespace Jellyfin.Plugin.LocalRecs.Tests.Unit.VirtualLibrary
                 }
             };
 
-            var mockLibraryManager = new Mock<ILibraryManager>();
-            mockLibraryManager.Setup(m => m.GetVirtualFolders()).Returns(existingFolders);
+                var mockLibraryManager = new Mock<ILibraryManager>();
+                mockLibraryManager.Setup(m => m.GetVirtualFolders()).Returns(existingFolders);
 
-            var user1 = new User("User1", "Default", "Default") { Id = user1Id };
-            var user2 = new User("User2", "Default", "Default") { Id = user2Id };
+                var user1 = new User("User1", "Default", "Default") { Id = user1Id };
+                var user2 = new User("User2", "Default", "Default") { Id = user2Id };
 
-            var mockUserManager = new Mock<IUserManager>();
-            mockUserManager.Setup(m => m.GetUsers()).Returns(new[] { user1, user2 });
-            mockUserManager.Setup(m => m.UpdateUserAsync(It.IsAny<User>())).Returns(Task.CompletedTask);
+                var mockUserManager = new Mock<IUserManager>();
+                mockUserManager.Setup(m => m.GetUsers()).Returns(new[] { user1, user2 });
+                mockUserManager.Setup(m => m.UpdateUserAsync(It.IsAny<User>())).Returns(Task.CompletedTask);
 
-            var service = CreateService(basePath, mockLibraryManager.Object, mockUserManager.Object);
+                var service = CreateService(basePath, mockLibraryManager.Object, mockUserManager.Object);
 
-            await service.SyncPermissionsForAllUsersAsync();
+                await service.SyncPermissionsForAllUsersAsync();
 
-            user1.HasPermission(PermissionKind.EnableAllFolders).Should().BeTrue();
-            user2.HasPermission(PermissionKind.EnableAllFolders).Should().BeTrue();
+                user1.HasPermission(PermissionKind.EnableAllFolders).Should().BeTrue();
+                user2.HasPermission(PermissionKind.EnableAllFolders).Should().BeTrue();
 
-            user1.GetPreference(PreferenceKind.BlockedMediaFolders)
-                .Should().Contain(user2MovieFolderId)
-                .And.NotContain(user1MovieFolderId);
+                user1.GetPreference(PreferenceKind.BlockedMediaFolders)
+                    .Should().Contain(user2MovieFolderId)
+                    .And.NotContain(user1MovieFolderId);
 
-            user2.GetPreference(PreferenceKind.BlockedMediaFolders)
-                .Should().Contain(user1MovieFolderId)
-                .And.NotContain(user2MovieFolderId);
+                user2.GetPreference(PreferenceKind.BlockedMediaFolders)
+                    .Should().Contain(user1MovieFolderId)
+                    .And.NotContain(user2MovieFolderId);
             }
         }
 
@@ -174,12 +174,12 @@ namespace Jellyfin.Plugin.LocalRecs.Tests.Unit.VirtualLibrary
         {
             using (UsePluginConfiguration(config => config.AutoCreateRecommendationLibraries = true))
             {
-            var basePath = CreateBasePath();
-            var userId = Guid.NewGuid();
-            var moviePath = Path.Combine(basePath, userId.ToString(), "movies");
-            var tvPath = Path.Combine(basePath, userId.ToString(), "tv");
+                var basePath = CreateBasePath();
+                var userId = Guid.NewGuid();
+                var moviePath = Path.Combine(basePath, userId.ToString(), "movies");
+                var tvPath = Path.Combine(basePath, userId.ToString(), "tv");
 
-            var existingFolders = new List<VirtualFolderInfo>
+                var existingFolders = new List<VirtualFolderInfo>
             {
                 new VirtualFolderInfo
                 {
@@ -195,18 +195,18 @@ namespace Jellyfin.Plugin.LocalRecs.Tests.Unit.VirtualLibrary
                 }
             };
 
-            var mockLibraryManager = new Mock<ILibraryManager>();
-            mockLibraryManager.Setup(m => m.GetVirtualFolders()).Returns(existingFolders);
-            mockLibraryManager
-                .Setup(m => m.RemoveVirtualFolder(It.IsAny<string>(), It.IsAny<bool>()))
-                .Returns(Task.CompletedTask);
+                var mockLibraryManager = new Mock<ILibraryManager>();
+                mockLibraryManager.Setup(m => m.GetVirtualFolders()).Returns(existingFolders);
+                mockLibraryManager
+                    .Setup(m => m.RemoveVirtualFolder(It.IsAny<string>(), It.IsAny<bool>()))
+                    .Returns(Task.CompletedTask);
 
-            var service = CreateService(basePath, mockLibraryManager.Object, Mock.Of<IUserManager>());
+                var service = CreateService(basePath, mockLibraryManager.Object, Mock.Of<IUserManager>());
 
-            await service.RemoveLibrariesForUserAsync(userId, "Alice");
+                await service.RemoveLibrariesForUserAsync(userId, "Alice");
 
-            mockLibraryManager.Verify(m => m.RemoveVirtualFolder("Alice's Recommended Movies", false), Times.Once);
-            mockLibraryManager.Verify(m => m.RemoveVirtualFolder("Alice's Recommended TV", false), Times.Once);
+                mockLibraryManager.Verify(m => m.RemoveVirtualFolder("Alice's Recommended Movies", false), Times.Once);
+                mockLibraryManager.Verify(m => m.RemoveVirtualFolder("Alice's Recommended TV", false), Times.Once);
             }
         }
 
